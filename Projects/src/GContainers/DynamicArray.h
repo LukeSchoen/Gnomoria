1be173d15986117a6c3aeddc0333333f9b41ad1a
PyroCybin <khan.maxfield@gmail.com>
2016-05-01 16:51:58 +1000

    wip

---
+++ Projects/src/GContainers/DynamicArray.h
@@ -0,0 +1,14 @@
+
+namespace gm
+{
+  template<typename T, size_t reallocationSize = 0>
+  struct DynamicArray
+  {
+    T* data;
+    size_t length;
+    size_t capacity;
+  };
+
+  template<typename 
+}
+
